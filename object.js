8f9839961471eefd93beb4ff8a232f612f3ee28d
0ladyshek <nicitaryzhakov@yandex.ru>
2022-03-16 15:26:05 +0300

    main

---
+++ object.js
@@ -0,0 +1,74 @@
+var GAME = {
+    width: 450,
+    height: 600,
+    background: "#252850",
+    score: 0,
+    pause: false,
+    level: 1,
+    lives: 5,
+    countBomb: 6,
+}
+
+var SHIP = {
+    image: `./images/ship/ship_${GAME.level}.png`,
+    img: new Image(),
+    load: false,
+    sizeX: 80,
+    sizeY: 120,
+    x: GAME.width / 2,
+    y: GAME.height - 120 - 20,
+    directionX: 5,
+}
+
+var BOMB = {
+    image: "./images/bomb.png",
+    y: -50,
+    directionY: 3,
+    sizeX: 40,
+    sizeY: 40,
+}
+
+var PACKET = {
+    image: `./images/packets/packet_${GAME.level}.png`,
+    sizeX: 15,
+    sizeY: 40,
+    directionY: 5,
+}
+
+var SETLIVE = {
+    image: "./images/live.png",
+    sizeX: 20,
+    sizeY: 20,
+    y: 10,
+}
+
+var STAR = {
+    R:  1.5,
+    color: 'white',
+    directionY: 2.5,
+}
+
+function Bomb() {
+    this.x = Math.floor(Math.random() * (GAME.width - 150)) + 50,
+    this.img = new Image(),
+    this.load = false,
+    this.y = -50
+}
+
+function Packet() {
+    this.img = new Image(),
+    this.x = SHIP.x + SHIP.sizeX / 2 - PACKET.sizeX / 2,
+    this.y = SHIP.y - PACKET.sizeY / 2,
+    this.load = false
+}
+
+function Live(x) {
+    this.img = new Image(),
+    this.x = x,
+    this.load = false
+}
+
+function Star() {
+    this.x = Math.floor(Math.random() * (GAME.width - 0)) + 50,
+    this.y = Math.floor(Math.random() * (GAME.height - 350)) + 0
+}
---
+++ script.js
@@ -0,0 +1,252 @@
+var canvas = document.getElementById("canvas");
+var canvasContext = canvas.getContext("2d");
+
+canvas.width = GAME.width
+canvas.height = GAME.height
+
+let bombs = [];
+let packets = [];
+let lives = [];
+let stars = [];
+
+function drawFrame() {
+    canvasContext.clearRect(0, 0, GAME.width, GAME.height);
+    drawBackground();
+    drawStars();
+    loadShip(SHIP);
+    loadPackets(packets);
+    loadBombs(bombs);
+    loadLives();
+    drawAll();
+}
+
+function drawBackground() {
+    canvasContext.fillStyle = GAME.background;
+    canvasContext.fillRect(0, 0, GAME.width, GAME.height);
+}
+
+function drawStars() {
+    for (var i = stars.length; i < 50; i++) {
+        stars.push(new Star())
+    }
+    for (let star of stars) {
+        canvasContext.fillStyle = STAR.color;
+        canvasContext.beginPath();
+        canvasContext.arc(star.x, star.y, STAR.R, 0, 2 * Math.PI);
+        canvasContext.fill();
+    }
+}
+
+function loadShip(ship) {
+    ship.img.src = ship.image
+    ship.img.onload = () => {ship.load = true}
+}
+
+function loadPackets(packets) {
+    for (let packet of packets) {
+        packet.img.src = PACKET.image
+        packet.img.onload = () => {packet.load = true}
+    }
+}
+
+function loadBombs(bombs) {
+    for (let bomb of bombs) {
+        bomb.img.src = BOMB.image
+        bomb.img.onload = () => {bomb.load = true}
+    }
+}
+
+function loadLives() {
+    for (let live of lives) {
+        live.img.src = SETLIVE.image
+        live.img.onload = () => {live.load = true}
+    }
+}
+
+function drawAll() {
+    if (SHIP.load) {canvasContext.drawImage(SHIP.img, SHIP.x, SHIP.y, SHIP.sizeX, SHIP.sizeY)}
+    for (let packet of packets) {
+        if (packet.load) {
+            canvasContext.drawImage(packet.img, packet.x, packet.y, PACKET.sizeX, PACKET.sizeY)
+        }
+    } 
+    for (let bomb of bombs) {
+        if (bomb.load) {
+            canvasContext.drawImage(bomb.img, bomb.x, bomb.y, BOMB.sizeX, BOMB.sizeY)
+        }
+    }
+    for (let live of lives) {
+        if (live.load) {
+            canvasContext.drawImage(live.img, live.x, SETLIVE.y, SETLIVE.sizeX, SETLIVE.sizeY)
+        }
+    }
+}
+
+function updateShip(ship) {
+    ship.x += ship.directionX
+}
+
+function checkShip(game, ship) {
+    if ((ship.x + ship.sizeX >= game.width) || (ship.x <= 0)) {
+        ship.directionX = -ship.directionX
+    }
+}
+
+function updatePackets() {
+    for (let packet of packets) {
+        packet.y -= PACKET.directionY
+    }
+}
+
+function checkPackets() {
+    for (var i = 0; i <= packets.length - 1; i++) {
+        var packet = packets[i];
+        if (0 > packet.y) {
+            packets.splice(i, 1);
+        }
+        for (var x = 0; x <= bombs.length - 1; x++) {
+            var bomb = bombs[x]
+            if ((packet.y <= bomb.y + BOMB.sizeY) && (packet.y >= bomb.y) && (packet.x >= bomb.x) && (packet.x <= bomb.x + BOMB.sizeX)) {
+                bombs.splice(x, 1);
+                packets.splice(i, 1)
+            }
+        }
+    }
+}
+
+function checkBomb(ship, game) {
+    for (var i = bombs.length; i < GAME.countBomb; i++) {
+        bombs.push(new Bomb());
+    }
+    for (var i = 0; i <= bombs.length - 1; i++) {
+        var bomb = bombs[i];
+        if (bomb.y >= GAME.height) {
+            bombs.splice(i, 1);
+        }
+        if ((bomb.x >= ship.x - BOMB.sizeX) && (bomb.x <= ship.x + ship.sizeX) && (bomb.y <= ship.y + ship.sizeY) && (bomb.y >= ship.y - ship.sizeY / 2 + BOMB.sizeY / 2)) {
+            bombs.splice(i, 1);
+            game.lives--;
+        }
+    }
+}
+
+function updateBomb() {
+    for (let bomb of bombs) {
+        bomb.y += BOMB.directionY
+    }
+}
+
+function checkLives() {
+    for (var i = lives.length; i < GAME.lives; i++) {
+        var downLive = lives[lives.length - 1]
+        if (downLive) {lives.push(new Live(downLive.x + 5 + SETLIVE.sizeX))}
+        if (!downLive) {lives.push(new Live(10))}
+    }
+    if(GAME.lives < lives.length + 1) {
+        lives.splice(GAME.lives, lives.length - GAME.lives)
+    }
+}
+
+function checkStars() {
+    for (var i = 0; i < stars.length - 1; i++) {
+        var star = stars[i]
+        if (star.y >= GAME.height) {
+            stars.splice(i, 1)
+        }
+    }
+}
+
+function updateStars() {
+    for (let star of stars) {
+        star.y += STAR.directionY
+    }
+}
+
+function gameOver() {
+    GAME.lives = 5;
+    bombs.length = 0;
+    packets.length = 0;
+    GAME.pause = true;
+}
+
+function play() {
+    if (!GAME.pause) {
+        drawFrame()
+        updateShip(SHIP)
+        checkShip(GAME, SHIP)
+        checkBomb(SHIP, GAME)
+        updateBomb()
+        updatePackets()
+        checkPackets()
+        checkLives()
+        updateStars()
+        checkStars()
+        if (GAME.lives == 0) {
+            drawFrame()
+            gameOver()
+        }
+    } else {
+        document.getElementById("title").innerHTML = "PAUSE"
+    }
+    requestAnimationFrame(play);
+}
+
+function moveMouse(event) {
+    SHIP.x = event.clientX
+    SHIP.directionX = 0
+    if (event.clientX >= GAME.width - SHIP.sizeX) {SHIP.x = GAME.width - SHIP.sizeX}
+}
+
+function checkKeyboard(event) {
+    if ((event.code == "Escape") || (event.code == "Enter")) {
+        GAME.pause = !GAME.pause
+    }
+    if (event.code == 'ArrowLeft') {
+        SHIP.directionX = -5 
+    }
+    if (event.code == 'ArrowRight') {
+        SHIP.directionX = 5
+    }
+    if (event.code == 'ArrowUp') {
+        packets.push(new Packet());
+    }
+}
+
+function checkMouse(event) {
+    if (event.button == 1) {
+        GAME.pause = !GAME.pause;
+    }
+    if (event.button == 0) {
+        packets.push(new Packet())
+    }
+}
+
+function checkTouch(event) {
+    if ((event.changedTouches[0].clientX >= 0) && (event.changedTouches[0].clientX <= GAME.width / 2 - 150)) {
+        SHIP.directionX = -5
+    }
+    if ((event.changedTouches[0].clientX > GAME.width / 2 + 150) && (event.changedTouches[0].clientX <= GAME.width)) {
+        SHIP.directionX = 5
+    } 
+    if((event.changedTouches[0].clientX > GAME.width / 2) && (event.changedTouches[0].clientX <= GAME.width / 2 + 150)) {
+        packets.push(new Packet());
+    }
+}
+
+function initListenerSingle() {
+    canvas.addEventListener('mousemove', moveMouse);
+    document.addEventListener('keydown', checkKeyboard);
+    document.addEventListener("mousedown", checkMouse);
+    document.addEventListener("touchstart", checkTouch);
+}
+
+function sleep(milliseconds) {       
+    const date = Date.now();        
+    let currentDate = null;       
+    do {               
+       currentDate = Date.now();      
+    } while (currentDate - date < milliseconds); 
+}  
+
+initListenerSingle();
+play()
